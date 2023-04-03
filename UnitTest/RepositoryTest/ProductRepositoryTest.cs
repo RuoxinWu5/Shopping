0c8f2dd4c5762ea667ea88a8edab3f5408232aab
ruoxin.wu <ruoxin.wu@thoughtworks.com>
2023-04-03 17:29:28 +0800

    test: AddProduct Repository Test.

--- UnitTest/RepositoryTest/ProductRepositoryTest.cs
+++ UnitTest/RepositoryTest/ProductRepositoryTest.cs
@@ -1,3 +1,4 @@
+using Data.Exceptions;
 using Data.Model;
 using Data.Repository;
 using Microsoft.EntityFrameworkCore;
@@ -24,8 +25,8 @@ namespace UnitTest.RepositoryTest
         {
             var products = new List<Product>
         {
-            new Product { name="Apple",quantity=100,sellerId=1 },
-            new Product {name="Banana",quantity=50,sellerId=1 }
+            new Product { name = "Apple", quantity = 100, sellerId = 1 },
+            new Product { name = "Banana", quantity = 50, sellerId = 1 }
         };
             await _context.AddRangeAsync(products);
             await _context.SaveChangesAsync();
@@ -42,6 +43,43 @@ namespace UnitTest.RepositoryTest
             var result = await _repository.GetProductListBySellerId(sellerId);
             // Assert
             Assert.Equal(products, result);
+        }
+
+        [Fact]
+        public async Task AddProduct_ShouldCreateNewProduct_WhenProductIsValid()
+        {
+            // Arrange
+            var products = await AddProducts();
+            var product = new Product { name = "Watermelon", quantity = 70, sellerId = 1 };
+            // Act
+            await _repository.AddProduct(product);
+            // Assert
+            Assert.NotNull(_context.Products);
+            var savedProduct = await _context.Products.FirstOrDefaultAsync(u => u.name == product.name);
+            Assert.NotNull(savedProduct);
+            Assert.Equal(product.name, savedProduct.name);
+            Assert.Equal(product.quantity, savedProduct.quantity);
+            Assert.Equal(product.sellerId, savedProduct.sellerId);
+        }
+
+        [Fact]
+        public async Task AddProduct_ShouldThrowDuplicateUserNameException_WhenProductNameExists()
+        {
+            // Arrange
+            var products = await AddProducts();
+            var product = new Product { name = "Banana", quantity = 50, sellerId = 1 };
+            // Act & Assert
+            await Assert.ThrowsAsync<DuplicateUserNameException>(async () => await _repository.AddProduct(product));
+        }
+
+        [Fact]
+        public async Task AddProduct_ShouldThrowNotFoundException_WhenSellerIdNotExists()
+        {
+            // Arrange
+            var products = await AddProducts();
+            var product = new Product { name = "Watermelon", quantity = 70, sellerId = 2 };
+            // Act & Assert
+            await Assert.ThrowsAsync<DllNotFoundException>(async () => await _repository.AddProduct(product));
         }
     }
 }
