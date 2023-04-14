6d785000415080f7b03008720d149d6e06a9aac4
ruoxin.wu <ruoxin.wu@thoughtworks.com>
2023-04-14 17:57:29 +0800

    test: Add Service Layer forgotten tests

--- UnitTest/ServiceTest/UserServiceTest.cs
+++ UnitTest/ServiceTest/UserServiceTest.cs
@@ -2,6 +2,7 @@ using Service;
 using Data.Repository;
 using Moq;
 using Data.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTest.ServiceTest
 {
@@ -15,6 +16,7 @@ namespace UnitTest.ServiceTest
             _userRepositoryMock = new Mock<IUserRepository>();
             _userService = new UserService(_userRepositoryMock.Object);
         }
+
         [Fact]
         public async Task AddUser_ShouldCallAddUserMethodOfRepository()
         {
@@ -26,5 +28,65 @@ namespace UnitTest.ServiceTest
             _userRepositoryMock.Verify(repository => repository.AddUser(user), Times.Once);
         }
 
+        [Fact]
+        public async Task GetUserById_ShouldCallGetUserByIdMethodOfRepository()
+        {
+            // Arrange
+            var id = 1;
+            // Act
+            await _userService.GetUserById(id);
+            // Assert
+            _userRepositoryMock.Verify(repository => repository.GetUserById(id), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetSellerById_ShouldReturnSellerInfo_WhenSellerIsfound()
+        {
+            // Arrange
+            var id = 1;
+            var seller = new User { Name = "Jack", Password = "Jack123", Type = UserType.SELLER };
+            _userRepositoryMock.Setup(repository => repository.GetUserById(It.IsAny<int>())).ReturnsAsync(seller);
+            // Act
+            var result = await _userService.GetSellerById(id);
+            // Assert
+            Assert.Equal(UserType.SELLER, result.Type);
+            _userRepositoryMock.Verify(repository => repository.GetUserById(id), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetSellerById_ShouldThrowNotFoundException_WhenSellerIsNotfound()
+        {
+            // Arrange
+            var id = 1;
+            var seller = new User { Name = "Jack", Password = "Jack123", Type = UserType.BUYER };
+            _userRepositoryMock.Setup(repository => repository.GetUserById(It.IsAny<int>())).ReturnsAsync(seller);
+            // Act & Assert
+            await Assert.ThrowsAsync<KeyNotFoundException>(async () => await _userService.GetSellerById(id));
+        }
+
+        [Fact]
+        public async Task GetBuyerById_ShouldReturnBuyerInfo_WhenBuyerIsfound()
+        {
+            // Arrange
+            var id = 1;
+            var seller = new User { Name = "Jack", Password = "Jack123", Type = UserType.BUYER };
+            _userRepositoryMock.Setup(repository => repository.GetUserById(It.IsAny<int>())).ReturnsAsync(seller);
+            // Act
+            var result = await _userService.GetBuyerById(id);
+            // Assert
+            Assert.Equal(UserType.BUYER, result.Type);
+            _userRepositoryMock.Verify(repository => repository.GetUserById(id), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetBuyerById_ShouldThrowNotFoundException_WhenBuyerIsNotfound()
+        {
+            // Arrange
+            var id = 1;
+            var seller = new User { Name = "Jack", Password = "Jack123", Type = UserType.SELLER };
+            _userRepositoryMock.Setup(repository => repository.GetUserById(It.IsAny<int>())).ReturnsAsync(seller);
+            // Act & Assert
+            await Assert.ThrowsAsync<KeyNotFoundException>(async () => await _userService.GetBuyerById(id));
+        }
     }
 }
