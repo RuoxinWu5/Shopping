d46b406d0e518d14e73b0058cd5133e07750b922
ruoxin.wu <ruoxin.wu@thoughtworks.com>
2023-04-25 13:52:28 +0800

    chore: data migration

---
+++ Data/Migrations/20230425023049_UpdateOrderStatus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOrderStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Orders",
+                newName: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Orders",
+                newName: "Type");
+        }
+    }
+}
